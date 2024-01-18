0a0cee2b8357a90dd4f60bc2cb9117a565c4d414
Morten Lunde <morten.lunde@live.no>
2024-01-18 19:24:03 +0100

    Added new project PythonEksamen.

---
+++ PythonEksamen/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PythonEksamen
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            string filePath = Path.Combine(Environment.CurrentDirectory, "solgtevarer.csv");
+
+            string sVarer = ReadFile(filePath);
+            Console.WriteLine(sVarer);
+        }
+        
+        private static string ReadFile(string filePath)
+        {
+            string varer = "";
+            string[] solgteVarer = File.ReadAllLines(filePath);
+
+            foreach (string s in solgteVarer)
+            {
+                varer += $"{s}";
+            }
+            
+            return varer;
+        }
+    }
+}
