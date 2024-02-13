19c5f9014d5cbb23927019b5b7de9574ff5fb570
Morten Lunde <morten.lunde@live.no>
2024-02-13 11:23:18 +0100

    new project weather2.0

---
+++ Weather/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Weather;
+
+public delegate bool WeatherWarning(double temperature);
+
+public static class Program
+{
+    private static bool FreezingWarning(double temperature)
+    {
+        return temperature <= 0;
+    }
+    private static bool SuperFreezingWarning(double temperature)
+    {
+        return temperature <= -20;
+    }
+    private static List<double> CheckTemperatures(List<double> temperatures, WeatherWarning warning)
+    {
+        return temperatures.Where(temp => warning(temp)).ToList();
+    }
+
+    private static void Main()
+    {
+        List<double> temperatures = [23, -23, -3, -2, 6, 11, 18];
+        
+        foreach (double temp in temperatures)
+        {
+            Console.WriteLine($"It is {temp} degrees");
+        }
+        List<double> freezingTemps = CheckTemperatures(temperatures, FreezingWarning);
+        foreach (double temp in freezingTemps)
+        {
+            Console.WriteLine($"It is freezing: {temp}");
+        }
+        List<double> superfreezingTemps = CheckTemperatures(temperatures, SuperFreezingWarning);
+        foreach (double temp in superfreezingTemps)
+        {
+            Console.WriteLine($"It is superfreezing: {temp}");
+        }
+    }
+}
---
+++ Weather2.0/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Wather2._0;
+
+public static class Program
+{
+    private delegate bool Warning(double temperature, double humidity,ref string message);
+    private static bool FreezingWarning(double temperature, double humidity, ref string message)
+    {
+        if (!(temperature <= 0)) return false;
+        message = "--- Freezing warning!!! ---";
+        return true;
+    }
+    static bool RainWarning(double temperature, double humidity,ref string message)
+    {
+        if (!(humidity >= 50)) return false;
+        message = "--- Rain warning!!! ---";
+        return true;
+    }
+
+    private static void Main()
+    {
+        List<(double Temperature, double Humidity)> weatherConditions =
+        [
+            (20.0, 40.0),
+            (10.0, 50.0),
+            (20.0, 50.0),
+            (-4, 15),
+            (0, 10),
+            (15, 75)
+            
+        ];
+        weatherConditions.Add((20.0, 50.0));
+
+        List<Warning> warnings =
+        [
+            FreezingWarning,
+            RainWarning
+        ];
+        
+        foreach ((double Temperature, double Humidity) condition in weatherConditions)
+        {
+            Console.WriteLine($"\nConditions \n" +
+                              $"Temp:{condition.Temperature}\n" +
+                              $"Humidity:{condition.Humidity}");
+            foreach (Warning warning in warnings)
+            {
+                string message = "";
+                if (warning(condition.Temperature, condition.Humidity, ref message))
+                {
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
