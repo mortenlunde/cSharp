6dcb543f7eb10cfb5bc1a2fe353bbb1b55f09833
Morten Lunde <morten.lunde@live.no>
2024-02-26 21:47:35 +0100

    updated with max- function in AverageNumbers

--- AverageNumbers/Program.cs
+++ AverageNumbers/Program.cs
@@ -1,6 +1,8 @@
-﻿List<int> nums = [2, 3, 2, 5, 3, 44, 34, 6, 7, 10];
+﻿List<int> nums = [2, 3, 2, 5, 3, 44, 34, 6, 7, 101, 10, 10, 10, 10];
 Average(nums);
 Median(nums);
+Mode(nums);
+Max((nums));
 return;
 
 void Average(IReadOnlyCollection<int> numbers)
@@ -19,4 +21,25 @@ void Median(IReadOnlyList<int> numbers)
         median /= 2;
     }
     Console.WriteLine($"Median: {median}");
+}
+
+void Mode(IEnumerable<int> numbers)
+{
+    Dictionary<int, int> modeCalc = new Dictionary<int, int>();
+    foreach (int number in numbers)
+    {
+        if (!modeCalc.TryAdd(number, 1))
+        {
+            modeCalc[number] += 1;
+        }
+    }
+    int modeValue = modeCalc.MaxBy(pair => pair.Value).Key;
+    int modeCount = modeCalc[modeValue];
+    Console.WriteLine($"Most used number: {modeValue} - Count: {modeCount} times.");
+}
+
+void Max(IEnumerable<int> numbers)
+{
+    Console.WriteLine(numbers.Max());
+    Console.WriteLine(numbers.Order().Last());
 }
