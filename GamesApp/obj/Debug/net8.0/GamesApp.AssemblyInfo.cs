77138425d5a06d5b9696da9a79d090054b10798f
Morten Lunde <morten.lunde@live.no>
2024-03-20 08:58:02 +0100

    update cache

--- GamesApp/obj/Debug/net8.0/GamesApp.AssemblyInfo.cs
+++ GamesApp/obj/Debug/net8.0/GamesApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5c81fd8145fd6b618c835f1e2bd646b6cd659c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0a9aca5f7ca1d8a1ddd5be558bf12197863718")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
